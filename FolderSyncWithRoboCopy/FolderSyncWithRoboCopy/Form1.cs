d82263e2f09d47bfd0d791afa01a9ec4bb32851f
hermann1812 <friembichler@outlook.com>
2020-05-10 11:59:28 +0200

    Release 1.0.0.0

---
+++ FolderSyncWithRoboCopy/FolderSyncWithRoboCopy/Form1.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Reflection;
+using System.Windows.Forms;
+
+namespace FolderSyncWithRoboCopy
+{
+    public partial class Form1 : Form
+    {
+        // Caption for MessageBox
+        readonly string caption = "One Way Folder Sync - " + Assembly.GetEntryAssembly().GetName().Version;
+
+        // Source and destination folder
+        string sourceFolder = string.Empty;
+        string destinationFolder = string.Empty;
+
+        // The last used directories will be saved in this text file
+        string txtFile = Path.Combine(Path.GetTempPath(), "OneWayFolderSync.txt");
+
+        // Errors will be logged in this text file
+        string logFile = Path.Combine(Path.GetTempPath(), "OneWayFolderSync.log");
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Text = caption;
+
+            // If available read the paths of last used directories
+            if (File.Exists(txtFile))
+            {
+                string[] lines = File.ReadAllLines(txtFile);
+
+                sourceFolder = lines[0];
+                destinationFolder = lines[1];
+            }
+            UpdateFileCount();
+        }
+
+        private void UpdateFileCount()
+        {
+            label_SourceFolder.Text = "Source Folder = " + sourceFolder;
+            label_DestinationFolder.Text = "Destination Folder = " + destinationFolder;
+
+            label_SourceAvailable.Text = CountFiles(sourceFolder) + " files currently available";
+            label_DestinationAvailable.Text = CountFiles(destinationFolder) + " files currently available";
+
+            if (CountFiles(sourceFolder) != CountFiles(destinationFolder))
+            {
+                button_StartSyncing.Enabled = true;
+            }
+            else
+            {
+                button_StartSyncing.Enabled = false;
+            }
+        }
+
+        private int CountFiles(string folder)
+        {
+            int fCount = 0;
+
+            if (Directory.Exists(folder))
+            {
+                try
+                {
+                    fCount = Directory.GetFiles(folder, "*", SearchOption.AllDirectories).Length;
+                }
+                catch (Exception ex)
+                {
+                    WriteToLogFile(logFile, ex);
+                }
+            }
+            return fCount;
+        }
+
+        private void button_ChangeSourceFolder_Click(object sender, EventArgs e)
+        {
+            var fbd = new FolderBrowserDialog();
+
+            DialogResult result = fbd.ShowDialog();
+
+            if (result == DialogResult.OK && !string.IsNullOrWhiteSpace(fbd.SelectedPath))
+            {
+                sourceFolder = fbd.SelectedPath;
+
+                label_SourceFolder.Text = "Source Folder = " + sourceFolder;
+            }
+            UpdateFileCount();
+        }
+
+        private void button_ChangeDestinationFolder_Click(object sender, EventArgs e)
+        {
+            var fbd = new FolderBrowserDialog();
+
+            DialogResult result = fbd.ShowDialog();
+
+            if (result == DialogResult.OK && !string.IsNullOrWhiteSpace(fbd.SelectedPath))
+            {
+                destinationFolder = fbd.SelectedPath;
+
+                label_DestinationFolder.Text = "Destination Folder = " + destinationFolder;
+            }
+            UpdateFileCount();
+        }
+
+        private void button_StartSyncing_Click(object sender, EventArgs e)
+        {
+            if (Directory.Exists(sourceFolder) && Directory.Exists(destinationFolder))
+            {
+                if (sourceFolder != destinationFolder)
+                {
+                    // During syncing button will be disabled
+                    //button_StartSyncing.Enabled = false;
+
+                    // Save the last used folder paths
+                    if (File.Exists(txtFile)) { File.Delete(txtFile); }
+                    File.WriteAllText(txtFile, sourceFolder + "\r\n" + destinationFolder);
+
+                    // Start syncing by using the Windows command "Robocopy"
+                    Process process = new Process();
+                    process.StartInfo.FileName = "robocopy";
+                    process.StartInfo.Arguments = " /MIR \"" + sourceFolder + "\" \"" + destinationFolder + "\"";
+
+                    //process.StartInfo.UseShellExecute = false;
+                    //process.StartInfo.CreateNoWindow = true;
+
+                    process.Start();
+                    process.WaitForExit();
+                }
+            }
+            else
+            {
+                MessageBox.Show("Please select valid directories!", caption, MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            UpdateFileCount();
+        }
+
+        private void WriteToLogFile(string logFile, Exception ex)
+        {
+            MessageBox.Show(ex.ToString(), caption, MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+            if (File.Exists(logFile))
+            {
+                File.AppendAllText(logFile, "\r\n=========> " + DateTime.Now + " <=========\r\n" + ex.ToString());
+            }
+            else
+            {
+                File.WriteAllText(logFile, "\r\n=========> " + DateTime.Now + " <=========\r\n" + ex.ToString());
+            }
+        }
+    }
+}
---
+++ FolderSyncWithRoboCopy/FolderSyncWithRoboCopy/Form1.designer.cs
@@ -0,0 +1,152 @@
+﻿namespace FolderSyncWithRoboCopy
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label_SourceFolder = new System.Windows.Forms.Label();
+            this.button_ChangeSourceFolder = new System.Windows.Forms.Button();
+            this.button_ChangeDestinationFolder = new System.Windows.Forms.Button();
+            this.label_DestinationFolder = new System.Windows.Forms.Label();
+            this.button_StartSyncing = new System.Windows.Forms.Button();
+            this.label_SourceAvailable = new System.Windows.Forms.Label();
+            this.label_DestinationAvailable = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label_SourceFolder
+            // 
+            this.label_SourceFolder.AutoSize = true;
+            this.label_SourceFolder.Location = new System.Drawing.Point(12, 9);
+            this.label_SourceFolder.Name = "label_SourceFolder";
+            this.label_SourceFolder.Size = new System.Drawing.Size(85, 13);
+            this.label_SourceFolder.TabIndex = 0;
+            this.label_SourceFolder.Text = "Source Folder = ";
+            // 
+            // button_ChangeSourceFolder
+            // 
+            this.button_ChangeSourceFolder.Location = new System.Drawing.Point(12, 25);
+            this.button_ChangeSourceFolder.Name = "button_ChangeSourceFolder";
+            this.button_ChangeSourceFolder.Size = new System.Drawing.Size(143, 23);
+            this.button_ChangeSourceFolder.TabIndex = 1;
+            this.button_ChangeSourceFolder.Text = "Change Source Folder";
+            this.button_ChangeSourceFolder.UseVisualStyleBackColor = true;
+            this.button_ChangeSourceFolder.Click += new System.EventHandler(this.button_ChangeSourceFolder_Click);
+            // 
+            // button_ChangeDestinationFolder
+            // 
+            this.button_ChangeDestinationFolder.Location = new System.Drawing.Point(12, 83);
+            this.button_ChangeDestinationFolder.Name = "button_ChangeDestinationFolder";
+            this.button_ChangeDestinationFolder.Size = new System.Drawing.Size(143, 23);
+            this.button_ChangeDestinationFolder.TabIndex = 3;
+            this.button_ChangeDestinationFolder.Text = "Change Destination Folder";
+            this.button_ChangeDestinationFolder.UseVisualStyleBackColor = true;
+            this.button_ChangeDestinationFolder.Click += new System.EventHandler(this.button_ChangeDestinationFolder_Click);
+            // 
+            // label_DestinationFolder
+            // 
+            this.label_DestinationFolder.AutoSize = true;
+            this.label_DestinationFolder.Location = new System.Drawing.Point(12, 67);
+            this.label_DestinationFolder.Name = "label_DestinationFolder";
+            this.label_DestinationFolder.Size = new System.Drawing.Size(104, 13);
+            this.label_DestinationFolder.TabIndex = 2;
+            this.label_DestinationFolder.Text = "Destination Folder = ";
+            // 
+            // button_StartSyncing
+            // 
+            this.button_StartSyncing.Location = new System.Drawing.Point(12, 129);
+            this.button_StartSyncing.Name = "button_StartSyncing";
+            this.button_StartSyncing.Size = new System.Drawing.Size(80, 23);
+            this.button_StartSyncing.TabIndex = 4;
+            this.button_StartSyncing.Text = "Start Syncing";
+            this.button_StartSyncing.UseVisualStyleBackColor = true;
+            this.button_StartSyncing.Click += new System.EventHandler(this.button_StartSyncing_Click);
+            // 
+            // label_SourceAvailable
+            // 
+            this.label_SourceAvailable.AutoSize = true;
+            this.label_SourceAvailable.Location = new System.Drawing.Point(161, 30);
+            this.label_SourceAvailable.Name = "label_SourceAvailable";
+            this.label_SourceAvailable.Size = new System.Drawing.Size(126, 13);
+            this.label_SourceAvailable.TabIndex = 5;
+            this.label_SourceAvailable.Text = "xx files currently available";
+            // 
+            // label_DestinationAvailable
+            // 
+            this.label_DestinationAvailable.AutoSize = true;
+            this.label_DestinationAvailable.Location = new System.Drawing.Point(161, 88);
+            this.label_DestinationAvailable.Name = "label_DestinationAvailable";
+            this.label_DestinationAvailable.Size = new System.Drawing.Size(126, 13);
+            this.label_DestinationAvailable.TabIndex = 6;
+            this.label_DestinationAvailable.Text = "xx files currently available";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(98, 134);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(218, 13);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Files may be deleted in the destination folder.";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(560, 163);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label_DestinationAvailable);
+            this.Controls.Add(this.label_SourceAvailable);
+            this.Controls.Add(this.button_StartSyncing);
+            this.Controls.Add(this.button_ChangeDestinationFolder);
+            this.Controls.Add(this.label_DestinationFolder);
+            this.Controls.Add(this.button_ChangeSourceFolder);
+            this.Controls.Add(this.label_SourceFolder);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "One Way Folder Sync";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label_SourceFolder;
+        private System.Windows.Forms.Button button_ChangeSourceFolder;
+        private System.Windows.Forms.Button button_ChangeDestinationFolder;
+        private System.Windows.Forms.Label label_DestinationFolder;
+        private System.Windows.Forms.Button button_StartSyncing;
+        private System.Windows.Forms.Label label_SourceAvailable;
+        private System.Windows.Forms.Label label_DestinationAvailable;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
